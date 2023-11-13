fce781c69ca053a591a9e9479761011d997dff51
Chiara Manni <chiaramiren@gmail.com>
2023-11-13 16:08:14 +0100

    trainer list is working

--- routes/trainer.routes.js
+++ routes/trainer.routes.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const User = require("../models/User.model")
+const { isLoggedIn } = require('../middleware/route-guard')
 
-//routes
+router.get("/", isLoggedIn, (req, res, next) => {
+    console.log("-----------------------------------")
 
+    User
+        .find({ role: { $ne: "Admin" } })
+        .then(trainers => res.render("trainers/list", { trainers }))
+        .catch(err => next(err))
+
+})
 
 module.exports = router
